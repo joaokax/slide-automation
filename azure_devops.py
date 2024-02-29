a897f0ac290adb1d78ed4a0642a31414eccc8730
Joao Kax <lukaxsilva.ls@gmail.com>
2024-02-29 18:28:12 -0300

    get tasks

--- azure_devops.py
+++ azure_devops.py
@@ -25,8 +25,7 @@ work_item_ids: list = []
 if work_item_list.work_items is not None:
     for work_item in work_item_list.work_items:
         work_item_ids.append(work_item.id)
-        # pprint.pprint(work_item.id)
-    # break
+
 pprint.pprint(work_item_ids)
 
 # GET WORK ITEM DETAILS
@@ -34,6 +33,22 @@ for work_item_id in work_item_ids:
     work_item = work_item_tracking_client.get_work_item(id=work_item_id)
     pprint.pprint(
         f"ID: {work_item.id}, Type: {work_item.fields['System.WorkItemType']}, Title: {work_item.fields['System.Title']}")
+
+
+# GET TASKS FROM WORK ITEM
+work_item_parent = 24984
+query_for_tasks = Wiql(query=f"SELECT [System.Id] "
+                             f"FROM WorkItems "
+                             f"WHERE [System.WorkItemType] = 'Task' "
+                             f"AND ([System.State] = 'Done' OR [System.State] = 'In Progress') "
+                             f"AND [System.Parent] = {work_item_parent}")
+task_list = work_item_tracking_client.query_by_wiql(query_for_tasks)
+
+task_ids: list = []
+if task_list.work_items is not None:
+    for task in task_list.work_items:
+        task_ids.append(task.id)
+pprint.pprint(f"lista de tasks {task_ids}")
 
 # USER PROJECT LIST
 # core_client = connection.clients.get_core_client()
