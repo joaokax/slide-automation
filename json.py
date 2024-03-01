c3b1ac07edadd2403beff96f45819d4647c0ba25
Joao Kax <lukaxsilva.ls@gmail.com>
2024-02-29 21:31:17 -0300

    json mock and type

---
+++ json.py
@@ -0,0 +1,255 @@
+azure_object_type = {
+    "project": "(PROJECT_NAME)",
+    "sprint": "(SPRINT_NUMBER)",
+    "work_items": [
+        {
+            "id": "(WORK_ITEM_ID)",
+            "type": "(WORK_ITEM_TYPE)",
+            "state": "(WORK_ITEM_STATE)",
+            "title": "(WORK_ITEM_TITLE)",
+            "tasks": [
+                {
+                    "task_id": "(TASK_ID)",
+                    "task_type": "(TASK_TYPE)",
+                    "task_state": "(TASK_STATE)",
+                    "task_title": "(TASK_TITLE)"
+                }
+            ]
+        }
+    ]
+
+}
+
+azure_object_mock = {
+    "project": "My Project",
+    "sprint": "44",
+    "work_items": [
+        {
+            "id": 18062,
+            "type": "Product Backlog Item",
+            "state": "Desenvolvendo",
+            "title": "Infra: CI/CD",
+            "tasks": [
+                {
+                    "task_id": 24961,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Actions AWS"
+                },
+                {
+                    "task_id": 24962,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Script de teste de conex\u00e3o"
+                }
+            ]
+        },
+        {
+            "id": 23521,
+            "type": "Product Backlog Item",
+            "state": "Done",
+            "title": "Informa\u00e7\u00e3o de novas formas de pagamento",
+            "tasks": [
+                {
+                    "task_id": 24926,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Deploy - Produ\u00e7\u00e3o "
+                }
+            ]
+        },
+        {
+            "id": 24190,
+            "type": "Product Backlog Item",
+            "state": "Done",
+            "title": "Adequa\u00e7\u00f5es Whitelabel - Pagamento",
+            "tasks": [
+                {
+                    "task_id": 24907,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Homologa\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 25012,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Deploy em produ\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 25013,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Merge e altera\u00e7\u00f5es necess\u00e1rias"
+                }
+            ]
+        },
+        {
+            "id": 24231,
+            "type": "Product Backlog Item",
+            "state": "Done",
+            "title": "Tela de informa\u00e7\u00f5es profissionais (Data Professional)",
+            "tasks": [
+                {
+                    "task_id": 24680,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Tela Informa\u00e7\u00f5es Profissionais refinada"
+                }
+            ]
+        },
+        {
+            "id": 24426,
+            "type": "Product Backlog Item",
+            "state": "Done",
+            "title": "SSO - Login Facebook",
+            "tasks": [
+                {
+                    "task_id": 24872,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Homologa\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 24972,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Produ\u00e7\u00e3o"
+                }
+            ]
+        },
+        {
+            "id": 24581,
+            "type": "Product Backlog Item",
+            "state": "New",
+            "title": "Adequa\u00e7\u00f5es whitelabel - cadastro usu\u00e1rio",
+            "tasks": [
+                {
+                    "task_id": 24735,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Adequa\u00e7\u00e3o do cadastro (tela e m\u00e9todo)"
+                }
+            ]
+        },
+        {
+            "id": 24860,
+            "type": "Product Backlog Item",
+            "state": "Desenvolvendo",
+            "title": "Tradu\u00e7\u00e3o de Rota(URL)",
+            "tasks": [
+                {
+                    "task_id": 24864,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Traduzindo rotas"
+                },
+                {
+                    "task_id": 25011,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Buscando e alterando URL/rotas hard coded sem tradu\u00e7\u00e3o"
+                }
+            ]
+        },
+        {
+            "id": 24963,
+            "type": "Bug",
+            "state": "Done",
+            "title": "Fix - Cupom de plano",
+            "tasks": [
+                {
+                    "task_id": 24964,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Investiga\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 24965,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Implementa\u00e7\u00e3o da solu\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 24966,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Deploy"
+                }
+            ]
+        },
+        {
+            "id": 24970,
+            "type": "Product Backlog Item",
+            "state": "New",
+            "title": "Adequa\u00e7\u00f5es whitelabel - cadastro de produto",
+            "tasks": []
+        },
+        {
+            "id": 24973,
+            "type": "Product Backlog Item",
+            "state": "Desenvolvendo",
+            "title": "SEO - Performance de carregamento das p\u00e1ginas",
+            "tasks": [
+                {
+                    "task_id": 24974,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Identifica\u00e7\u00e3o e listagem de melhorias de performance"
+                }
+            ]
+        },
+        {
+            "id": 24984,
+            "type": "Product Backlog Item",
+            "state": "Desenvolvendo",
+            "title": "[SEO] Corre\u00e7\u00f5es H1, Meta Descriptions Duplicados",
+            "tasks": [
+                {
+                    "task_id": 25025,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Adi\u00e7\u00e3o das tags H1"
+                },
+                {
+                    "task_id": 25026,
+                    "task_type": "Task",
+                    "task_state": "In Progress",
+                    "task_title": "Corre\u00e7\u00e3o das meta descriptions"
+                }
+            ]
+        },
+        {
+            "id": 25015,
+            "type": "Bug",
+            "state": "Done",
+            "title": "Fix: Erro ao realizar compra sem telefone ",
+            "tasks": [
+                {
+                    "task_id": 25016,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Investiga\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 25017,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Implementa\u00e7\u00e3o da solu\u00e7\u00e3o"
+                },
+                {
+                    "task_id": 25018,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Realizando testes"
+                },
+                {
+                    "task_id": 25019,
+                    "task_type": "Task",
+                    "task_state": "Done",
+                    "task_title": "Deploy em produ\u00e7\u00e3o"
+                }
+            ]
+        }
+    ]
+}
